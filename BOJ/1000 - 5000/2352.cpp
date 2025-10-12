13a8fc07a3376b5a802524538a4ef1300035e36e
alluminized <allumin.official@gmail.com>
2025-10-13 08:52:05 +0900

    frgtju

---
+++ BOJ/1000 - 5000/2352.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+  int a, n;
+  vector<int> arr;
+  arr.push_back(-1);
+  cin >> a;
+  for (int i = 0; i < a; i++) {
+    cin >> n;
+    if (arr.back() < n) {
+      arr.push_back(n);
+    } else {
+      arr[lower_bound(arr.begin(), arr.end(), n) - arr.begin()] = n;
+    }
+  }
+  cout << arr.size() - 1 << "\n";
+  return 0;
+}
